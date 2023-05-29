75e533c12a381728772a3bdfd1d678dfa5acda91
RenatuRufus <renatooffice1@gmail.com>
2023-05-29 19:40:18 -0300

    1

---
+++ code/1 - INICIO/dados_2.js
@@ -0,0 +1,28 @@
+let preco = 19.90
+let desconto = 0.4
+
+console.log(19.9 * 0.6) //valor literal
+
+console.log("preço e desconto\n")
+let precoDesconto = preco * (1 - desconto)
+console.log(precoDesconto)
+
+let nome = "Caderno"
+let categoria = "Papelaria"
+console.log(nome +" "+ categoria)
+console.log("Produto: " + nome + ", Categoria: " + categoria)
+
+console.log("\n\n")
+console.log("Produto: " + nome
+            + ", Categoria: " + categoria
+            + ", Preço: " + preco
+            + ", Desconto: " + desconto)
+
+//String (texto)
+
+console.log(1+1)
+console.log("1" + 1) //concatenação
+
+
+
+
---
+++ code/1 - INICIO/desafio1.js
@@ -0,0 +1,26 @@
+/*
+ *   Desafio bloco sentença
+     exibir números pares e impares
+     bloco {1,3,5,7,9}
+     bloco {2,4,6,8,10} 
+ */
+
+// 1
+// 2
+// 3
+// 4     
+{
+    console.log(1)
+    console.log(3)
+    console.log(5)
+    console.log(7)
+}
+
+{
+    console.log(2)
+    console.log(4)
+    console.log(6)
+    console.log(8)
+}
+
+

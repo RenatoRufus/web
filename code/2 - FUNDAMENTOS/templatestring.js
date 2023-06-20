afa1bc5a561a3a780ba74d7d3786cc4200b32b3f
RenatuRufus <renatooffice1@gmail.com>
2023-06-19 22:06:19 -0300

    19

---
+++ code/2 - FUNDAMENTOS/templatestring.js
@@ -0,0 +1,14 @@
+const nome = 'Rebeca'
+const concatenacao = "Rebeca" + nome + '!'
+
+const template = `
+                 Olá
+                 ${nome}!`
+console.log(concatenacao, template)
+
+//expressões...
+console.log(`1 + 1 = ${1 + 1}`)
+
+const up = texto => texto.toUpperCase()
+console.log(`Ei... ${up('cuidado')}!`)
+

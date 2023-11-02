9faa1bec9a29c9d559e6a17379bc6bbe76ebb856
RenatoRufus <renatooffice1@gmail.com>
2023-11-01 21:15:50 -0300

    .

--- code/1 - INICIO/dados_3.js
+++ code/1 - INICIO/dados_3.js
@@ -21,3 +21,6 @@ console.log(typeof flutuante)
 
 
 
+
+
+
--- code/1 - INICIO/dados_4.js
+++ code/1 - INICIO/dados_4.js
@@ -1,11 +1,35 @@
-let a = 3
+/*let a = 3
 //....
 a = a + 10
 //...
 console.log(a)
 
-//********************//
-const b = 100
+/********************/
+//const b = 100
+
+let a = 3
+
+a = 10 
+console.log(a)
+
+a = a + 10;
+console.log(a)
+
+const b = 10000
+console.log(b)
+
+//b = b + 1  erro tentar alterar constante
+
+
+
+
+
+
+
+
+
+
+
 
 
 

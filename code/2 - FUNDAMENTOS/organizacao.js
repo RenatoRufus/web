274e110a748749dbf69477df99f2c5d14f98faa1
RenatoRufus <renatooffice1@gmail.com>
2023-11-06 20:49:28 -0300

    .

--- code/2 - FUNDAMENTOS/organizacao.js
+++ code/2 - FUNDAMENTOS/organizacao.js
@@ -6,7 +6,11 @@ console.log("Sentença de código")
 
 {
     {
-        console.log("Olá bloco")
+        console.log("Olá bloco\n")
+        console.log("Hello\n")
+        console.log("World!\n")
     }
 }
 console.log("Olá mundo")
+
+console.log("Sentença de código")

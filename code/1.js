6e23446c5d8120158cda779baf6ab2b6b9aaff2e
RenatoRufus <renatooffice1@gmail.com>
2024-04-02 21:02:02 -0300

    .

--- code/1.js
+++ code/1.js
@@ -1,6 +1,3 @@
 
 console.log("legal")
 console.log("Teste");
-
-print(123)
-console.log(1+1)
---
+++ code/2.js
@@ -0,0 +1,38 @@
+//Sentença
+//Bloco de código
+
+/*
+Livro -
+    Letras
+    Parágrafos
+    Seções
+    Capítulos
+
+    para no final criar o livro
+
+
+Uma sentença de código pode ter um "";"" ou não.
+exemplo: console.log("Legal!!!")
+         console.log("Legal!!!");
+         
+
+*/
+
+
+console.log("Hello world!!!!")
+console.log("Boa noite!")
+console.log(1)
+console.log(1+2)
+console.log("Hello world!!!!\n Hello Melow Rob \n Dididi é")
+
+
+
+
+
+
+
+
+
+
+
+

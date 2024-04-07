93d1673b970281810bcef4c2de5fac4e446700ac
RenatoRufus <renatooffice1@gmail.com>
2024-04-06 23:06:23 -0300

    .

---
+++ 6.js
@@ -0,0 +1,29 @@
+console.log("Caneta")
+
+var nome = "Caneta"
+
+var quantidade  = 10
+console.log(quantidade)
+
+var preco = 6.4
+console.log(preco)
+
+var imposto = 1.5
+console.log(imposto)
+
+console.log(preco + imposto)
+
+var precofinal = preco + imposto
+console.log(precofinal)
+
+console.log(nome)
+
+
+
+
+
+
+
+
+
+

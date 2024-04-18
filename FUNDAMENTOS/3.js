33a2f6635cd0f267693b9011280b598d11fe8d8f
RenatoRufus <renatooffice1@gmail.com>
2024-04-17 21:40:07 -0300

    .

--- FUNDAMENTOS/3.js
+++ FUNDAMENTOS/3.js
@@ -11,7 +11,11 @@ const avaliacao2  = 6.871
 const total = avaliacao1 * peso1 + avaliacao2 * peso2
 const media = total / ( peso1 + peso2)
 
-console.log(media.toFixed(2))
+console.log(media.toFixed(2)) //CASA DECIMAIS 2 IMPRIMIR
+
+console.log(media.toString()) //Transforma em string
+console.log(media.toString(2)) //Transforma em string
+console.log(typeof media)
 
 
 
---
+++ FUNDAMENTOS/4.js
@@ -0,0 +1,8 @@
+//Coisas estranhas
+
+console.log(7 / 0)
+console.log("10" / 2) //Não faça essa merda!
+console.log("Show!" * 2)
+console.log(0.1 + 0.7)
+// erro console.log(10.toString)
+console.log((10.345).toFixed(2))

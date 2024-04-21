68da92c0de93f7fa64f71392d2791f8b9b5f7cb4
RenatoRufus <renatooffice1@gmail.com>
2024-04-21 19:41:18 -0300

    .

---
+++ FUNDAMENTOS/7.js
@@ -0,0 +1,21 @@
+const nome = 'Rebeca'
+const concatenação = 'Olá ' + nome + '!'
+
+console.log(nome)
+console.log(concatenação)
+
+const template = `
+Olá
+${nome}!
+`
+
+//expressões
+console.log(`1 + 1 = ${ 1 + 1 }`)
+
+
+const up = texto => texto.toUpperCase()
+console.log(`Ei... ${up('cuidado')}!`)
+
+
+
+

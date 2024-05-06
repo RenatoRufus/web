104eee8cbecfc831c74034324c5fc8f5b0822189
RenatoRufus <renatooffice1@gmail.com>
2024-05-05 21:29:16 -0300

    .

---
+++ FUNDAMENTOS/12.js
@@ -0,0 +1,20 @@
+// objeto
+
+const prod1 = {} //objeto vazio
+prod1.nome = 'Celular'
+prod1.preco = 10.90
+prod1['Desconto legal'] = 0.9 //evitar atributos com espaço
+
+console.log(prod1)
+
+
+const prod2 = {
+    nome: 'Camisa',
+    preco: 79.01
+
+}
+
+console.log(prod2)
+
+
+//pesquisar json

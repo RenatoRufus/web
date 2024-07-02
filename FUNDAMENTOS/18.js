2aea7a6caa390f829aa5196281fd5251366b9c2a
RenatoRufus <renatooffice1@gmail.com>
2024-07-01 21:42:54 -0300

    .

---
+++ FUNDAMENTOS/18.js
@@ -0,0 +1,20 @@
+//Hosting
+
+console.log('a =', a)
+var a = 2    //se comentar gera erro
+console.log('a =', a)
+
+
+function teste(){
+    console.log('a =',a)
+    var a = 2
+    console.log('a =', a)
+
+}
+teste()
+// console.log('a =', a) erro
+
+
+//com let o hoisting não funciona
+
+
---
+++ FUNDAMENTOS/19.js
@@ -0,0 +1,19 @@
+//objeto
+
+console.log(typeof Object)
+console.log(typeof new Object) //instânciando uma funçao
+
+const Cliente = function(){}
+console.log(typeof Cliente)
+console.log(typeof new Cliente)
+
+class Produto {} // ES 2015 (ES6)
+console.log(typeof Produto)
+console.log(typeof new Produto())
+
+
+//criar objeto literal
+
+
+
+
